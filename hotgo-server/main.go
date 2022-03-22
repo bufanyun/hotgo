4dc278f9cb1742dc3a7ee9c07d16ed426e1eee80
孟帅 <133814250@qq.com>
2022-03-22 09:45:18 +0800

    更新运行时致命错误打印堆栈

--- hotgo-server/main.go
+++ hotgo-server/main.go
@@ -2,17 +2,16 @@ package main
 
 import (
 	"github.com/bufanyun/hotgo/boot"
+	"github.com/gogf/gf/v2/errors/gerror"
 	"github.com/gogf/gf/v2/frame/g"
 	"github.com/gogf/gf/v2/os/gctx"
 )
 
 func main() {
 
-	var (
-		ctx = gctx.New()
-	)
+	var ctx = gctx.New()
 
 	if err := boot.Main.RunWithError(ctx); err != nil {
-		g.Log().Fatal(ctx, err)
+		g.Log().Fatal(ctx, gerror.Stack(err))
 	}
 }
