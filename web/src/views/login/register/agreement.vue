fdefb42253d7fa9e561a248d49dc6390d3595a39
孟帅 <133814250@qq.com>
2023-05-15 11:39:12 +0800

    修改注册协议显示

--- web/src/views/login/register/agreement.vue
+++ web/src/views/login/register/agreement.vue
@@ -1,8 +1,12 @@
 <template>
   <div>
-    <n-checkbox v-model:checked="checked" class="text-14px">我已阅读并接受</n-checkbox>
-    <n-button :text="true" type="primary" @click="handleClickProtocol">《用户协议》</n-button>
-    <n-button :text="true" type="primary" @click="handleClickPolicy">《隐私权政策》</n-button>
+    <n-checkbox v-model:checked="checked" class="text-12px">我已阅读并接受</n-checkbox>
+    <n-button text type="primary" @click="handleClickProtocol" class="text-12px">
+      《用户协议》
+    </n-button>
+    <n-button text type="primary" @click="handleClickPolicy" class="text-12px">
+      《隐私权政策》
+    </n-button>
   </div>
 </template>
 
@@ -44,7 +48,7 @@
 </script>
 
 <style scoped>
-  .text-14px {
-    font-size: 14px;
+  .text-12px {
+    font-size: 12px;
   }
 </style>
