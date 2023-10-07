bd93d07e1dc68319071d462746ec7d8bb2bf9a31
Kevin <77494658+Kevin-Ben@users.noreply.github.com>
2023-10-07 10:20:39 +0800

    * delete hgexample

--- server/addons/hgexample/crons/crons.go
+++
@@ -1,9 +0,0 @@
-// Package crons
-// @Link  https://github.com/bufanyun/hotgo
-// @Copyright  Copyright (c) 2023 HotGo CLI
-// @Author  Ms <133814250@qq.com>
-// @License  https://github.com/bufanyun/hotgo/blob/master/LICENSE
-package crons
-
-// 定时任务.
-// 插件中的定时任务可以统一在这里注册和处理
--- server/addons/hgexample/queues/queues.go
+++
@@ -1,9 +0,0 @@
-// Package queues
-// @Link  https://github.com/bufanyun/hotgo
-// @Copyright  Copyright (c) 2023 HotGo CLI
-// @Author  Ms <133814250@qq.com>
-// @License  https://github.com/bufanyun/hotgo/blob/master/LICENSE
-package queues
-
-// 消息队列.
-// 插件中的消息队列消费者可以统一在这里注册和处理
