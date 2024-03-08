06131680ddb71bc8a5454c438b7a555d8977f80e
孟帅 <133814250@qq.com>
2024-03-08 10:34:37 +0800

    移除无用测试文件

--- server/internal/library/hggen/internal/cmd/testdata/genservice/logic/logic_expect.go
+++
@@ -1,9 +0,0 @@
-// ==========================================================================
-// Code generated and maintained by GoFrame CLI tool. DO NOT EDIT.
-// ==========================================================================
-
-package logic
-
-import (
-	_ "hotgo/internal/library/hggen/internal/cmd/testdata/genservice/logic/article"
-)
--- server/internal/library/hggen/internal/cmd/testdata/issue/2746/sql.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE %s (
-     `id` int unsigned NOT NULL AUTO_INCREMENT COMMENT 'User ID',
-     `nickname` varchar(45) NOT NULL COMMENT 'User Nickname',
-     `tag`  json NOT NULL,
-     `info` longtext DEFAULT NULL,
-     `tag2` json COMMENT 'Tag2',
-     PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
